2a15be68e37066b72f621fdaca11ef860f19d962
Leonardo Hugens <leonardohugens@protonmail.com>
2024-11-13 13:53:49 +0000

    first commit

--- TimeConversion/src/Solution.java
+++ TimeConversion/src/Solution.java
@@ -1,27 +1,26 @@
 public class Solution {
 
     public static String timeConversion(String s) {
-        // Write your code here
 
         String format = s.substring(s.length() - 2);
-        String time = s.substring(0, s.length() - 3);
+
         String hour = s.substring(0, 2);
         String minute = s.substring(3, 5);
-        String second = s.substring(6,8);
+        String second = s.substring(6, 8);
 
-        if(format.equals("AM")){
-            if(time.equals("12:00:00")){
-                return "00:00:00";
+        if (format.equals("AM")) {
+            if (hour.equals("12")) {
+                hour = "00";
             }
-            return time;
+        } else {
+            if (!hour.equals("12")) {
+                hour = String.valueOf(Integer.parseInt(hour) + 12);
+            }
         }
 
-        int hourInt = (Integer.parseInt(hour) + 12) % 24;
-
-        time = hourInt + ":" + minute + ":" + second;
-        return time;
-
+        return hour + ":" + minute + ":" + second;
     }
+
 
     public static void main(String[] args) {
 
